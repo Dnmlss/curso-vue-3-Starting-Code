6ec6e704c067fd1750c8b093499b806c9180a383
Diego Miño <diegominho92@gmail.com>
2023-10-05 23:55:21 -0300

    renderizado condicional & Atributos dinamicos

--- js/main.js
+++ js/main.js
@@ -7,13 +7,24 @@ const app = Vue.createApp({
 	data() {
 		return {
 			search: null, // Creamos una variable search para almacenar el valor del input
+			result: null, // Creamos una variable result para almacenar el resultado de la peticion
+			error: null, // Creamos una variable error para almacenar el error de la peticion
 		};
 	},
 	methods: {
 		async doSearch() {
-			const response = await fetch(API + this.search); // Hacemos la peticion a la API de github
-			const data = await response.json(); // Convertimos la respuesta en un objeto JSON
-			console.log(data);
+			this.result = this.error = null; // Limpiamos los valores de result y error
+			try {
+				const response = await fetch(API + this.search); // Hacemos la peticion a la API de github
+				if (!response.ok) throw new Error("User Not Found"); // Si la respuesta no es correcta lanzamos un error
+				const data = await response.json(); // Convertimos la respuesta en un objeto JSON
+				console.log(data);
+				this.result = data;
+			} catch (error) {
+				this.error = error; // Si hay un error lo almacenamos en la variable error
+			} finally {
+				this.search = null; // Limpiamos el valor del input
+			}
 		},
 	},
 });
