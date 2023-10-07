cff84a2d6cdd375b7f6a79d15226b78d624fd125
Diego Miño <diegominho92@gmail.com>
2023-10-07 02:10:30 -0300

    checkpoint

--- js/main.js
+++ js/main.js
@@ -9,8 +9,29 @@ const app = Vue.createApp({
 			search: null, // Creamos una variable search para almacenar el valor del input
 			result: null, // Creamos una variable result para almacenar el resultado de la peticion
 			error: null, // Creamos una variable error para almacenar el error de la peticion
+			favorites: new Map(), // Creamos una variable favorites para almacenar los usuarios favoritos
 		};
 	},
+	created() {
+		const savedFavorites = JSON.parse(
+			window.localStorage.getItem("favorites")
+		); // Obtenemos los favoritos del localStorage
+		if (savedFavorites) {
+			const favorites = new Map(
+				savedFavorites.map((favorite) => [favorite.id, favorite])
+			);
+			this.favorites = favorites;
+		}
+	},
+	computed: {
+		isFavorite() {
+			return this.favorites.has(this.result.id);
+		},
+		allFavorites() {
+			return Array.from(this.favorites.values());
+		},
+	},
+
 	methods: {
 		async doSearch() {
 			this.result = this.error = null; // Limpiamos los valores de result y error
@@ -25,6 +46,25 @@ const app = Vue.createApp({
 			} finally {
 				this.search = null; // Limpiamos el valor del input
 			}
+		},
+		addFavorite() {
+			this.favorites.set(this.result.id, this.result); // Añadimos el usuario a la lista de favoritos
+			this.updateFavorite(); // Actualizamos la lista de favoritos en el localStorage
+		},
+		removeFavorite() {
+			this.favorites.delete(this.result.id);
+			this.updateFavorite();
+		},
+
+		showFavorites(favorite) {
+			this.result = favorite;
+		},
+
+		updateFavorite() {
+			window.localStorage.setItem(
+				"favorites",
+				JSON.stringify(this.allFavorites)
+			);
 		},
 	},
 });
