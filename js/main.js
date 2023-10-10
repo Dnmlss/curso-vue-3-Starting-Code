831ec981ad0d7bbca090ae87c1fcf8886f52e557
Diego Miño <diegominho92@gmail.com>
2023-10-10 14:20:15 -0300

    Transiciones & cachear favoritos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const API = "https://api.github.com/users/";
 
-// Creamos una funcion asincrona para hacer la peticion a la API de github
+const requestMaxTimeMs = 3000;
 
 // Utilizamos el metodo createApp para crear una instancia de Vue
 const app = Vue.createApp({
@@ -18,14 +18,14 @@ const app = Vue.createApp({
 		); // Obtenemos los favoritos del localStorage
 		if (savedFavorites) {
 			const favorites = new Map(
-				savedFavorites.map((favorite) => [favorite.id, favorite])
+				savedFavorites.map((favorite) => [favorite.login, favorite])
 			);
 			this.favorites = favorites;
 		}
 	},
 	computed: {
 		isFavorite() {
-			return this.favorites.has(this.result.id);
+			return this.favorites.has(this.result.login);
 		},
 		allFavorites() {
 			return Array.from(this.favorites.values());
@@ -35,12 +35,31 @@ const app = Vue.createApp({
 	methods: {
 		async doSearch() {
 			this.result = this.error = null; // Limpiamos los valores de result y error
+
+			const foundInFavorites = this.favorites.get(this.search); // Buscamos si el usuario ya esta en favoritos
+
+			const shouldRequestAgain = (() => {
+				if (!!foundInFavorites) {
+					const { lastRequestTime } = foundInFavorites;
+					const now = Date.now();
+					return now - lastRequestTime > requestMaxTimeMs;
+				}
+				return false;
+			})(); // IIFE
+
+			if (!!foundInFavorites && !shouldRequestAgain) {
+				console.log("Found and we use the cached version");
+				return (this.result = foundInFavorites);
+			} // Si el usuario esta en favoritos, lo mostramos y salimos de la funcion
+
 			try {
+				console.log("Not found or cached version is too old");
 				const response = await fetch(API + this.search); // Hacemos la peticion a la API de github
 				if (!response.ok) throw new Error("User Not Found"); // Si la respuesta no es correcta lanzamos un error
 				const data = await response.json(); // Convertimos la respuesta en un objeto JSON
 				console.log(data);
 				this.result = data;
+				foundInFavorites.lastRequestTime = Date.now();
 			} catch (error) {
 				this.error = error; // Si hay un error lo almacenamos en la variable error
 			} finally {
@@ -48,11 +67,12 @@ const app = Vue.createApp({
 			}
 		},
 		addFavorite() {
-			this.favorites.set(this.result.id, this.result); // Añadimos el usuario a la lista de favoritos
+			this.result.lastRequestTime = Date.now();
+			this.favorites.set(this.result.login, this.result); // Añadimos el usuario a la lista de favoritos. primero su id, luego el objeto completo
 			this.updateFavorite(); // Actualizamos la lista de favoritos en el localStorage
 		},
 		removeFavorite() {
-			this.favorites.delete(this.result.id);
+			this.favorites.delete(this.result.login);
 			this.updateFavorite();
 		},
 
